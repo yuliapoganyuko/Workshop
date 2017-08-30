5602ad52786824481921b315660aa8e049beaa96
Yuliya Pahaniuka <Yuliya_Pahaniuka@epam.com>
2017-08-30 11:43:14 +0300

    add sevice interface

---
+++ Service.Interface/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface IService<T>
+    {
+        IEnumerable<T> GetItems(int id);
+        void DeleteItem(int id);
+        void UpdateItem(T item);
+        void CreateItem(T item);
+    }
+}
