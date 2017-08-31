db8759ed7a981ac5346ac0bf9674bdfa96eb269d
Your Name <yuliapoganyuko@gmail.com>
2017-08-31 05:52:43 +0300

    add storage

---
+++ Service.Interface/Entities/PortfolioItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface.Entities
+{
+    public class PortfolioItem
+    {
+        public int ItemId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Symbol { get; set; }
+
+        public int SharesNumber { get; set; }
+    }
+}
